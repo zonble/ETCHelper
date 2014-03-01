193c8fc9e23c34e5b1f960dda45366f1de4e3b92
zonble <zonble@gmail.com>
2014-03-01 14:33:44 +0800

    Updates data.txt.

--- EtagHelperTests/EtagHelperTests.m
+++ EtagHelperTests/EtagHelperTests.m
@@ -41,6 +41,24 @@
 	}
 }
 
+- (void)testRoutesBetweenArbitraryNames
+{
+	for (NSInteger i = 0; i < 10; i++) {
+		NSArray *nodes = [[manager allNodeNames] allObjects];
+		NSString *from = [nodes objectAtIndex:arc4random() % [nodes count]];
+		NSString *to = nil;
+		while (to == nil) {
+			NSString *aNode = [nodes objectAtIndex:arc4random() % [nodes count]];
+			if (aNode != from) {
+				to = aNode;
+				break;
+			}
+		}
+		NSArray *routes = [manager possibleRoutesFrom:from to:to error:nil];
+		XCTAssertTrue([routes count] > 0, @"Must have at least a route");
+	}
+}
+
 - (void)testRoutesBetweenArbitraryNodes
 {
 	for (NSInteger i = 0; i < 10; i++) {
@@ -56,9 +74,9 @@
 		}
 		NSArray *routes = [manager possibleRoutesFromNode:from toNode:to error:nil];
 		XCTAssertTrue([routes count] > 0, @"Must have at least a route");
-//		for (ZBRoute *route in routes) {
-//			NSLog(@"%@", route.stringPresentation);
-//		}
+		for (ZBRoute *route in routes) {
+			NSLog(@"%@", route.stringPresentation);
+		}
 	}
 }
 
