0f260061928321b1215787e9d7d670b4f18beae7
zonble <zonble@gmail.com>
2014-03-01 22:36:07 +0800

    Updates README.md.

--- EtagHelper/main.m
+++ EtagHelper/main.m
@@ -1,13 +1,4 @@
-//
-//  main.m
-//  EtagHelper
-//
-//  Created by zonble on 2/26/14.
-//  Copyright (c) 2014 Weizhong Yang. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
-
 #import "ZBAppDelegate.h"
 
 int main(int argc, char * argv[])
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-EtagHelper
-==========
+ETCHelper
+=========
+
+An iOS tool which helps you to calculate how much does it cost to
+drive on freeways in Taiwan.
